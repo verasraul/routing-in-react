115101c2723a776e5ca759521a69e4ca9796b913
Raul Veras <verasraul@gmail.com>
2023-06-21 01:44:40 -0400

    adding screen components

---
+++ src/screens/Post.jsx
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+
+const Post = (props) => {
+  console.log(props);
+
+  const { post_id } = useParams();
+
+  const postNumber = parseInt(post_id) - 1;
+  const post = props[postNumber];
+
+  return (
+    <div className="post">
+      <div className="hero">
+        <img src={post.image} alt="post banner" />
+      </div>
+      <div className="content">
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
